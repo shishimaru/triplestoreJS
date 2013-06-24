a976b8b402ac8284e74237ff61cee731214f6d6a
Hitoshi Uchida <uchida@w3.org>
2013-06-24 15:31:40 -0700

    Re-import of Semantic Spider demo

---
+++ examples/demo/spider/chrome-ex/microdataJS/jquery.microdata.json.js
@@ -0,0 +1,50 @@
+/* -*- mode: js; js-indent-level: 2; indent-tabs-mode: nil -*- */
+
+'use strict';
+
+// http://www.whatwg.org/specs/web-apps/current-work/multipage/microdata.html#json
+jQuery.microdata.json = function(selector, format) {
+  var $ = jQuery;
+
+  function getObject(item, memory) {
+    var $item = $(item);
+    var result = {};
+    var types = $item.itemType();
+    if (types.length)
+      result.type = $(types).toArray();
+    if ($item.itemId())
+      result.id = $item.itemId();
+    result.properties = {};
+    $item.properties().each(function(i, elem) {
+      var $elem = $(elem);
+      var value;
+      if ($elem.itemScope()) {
+        if ($.inArray(elem, memory) != -1) {
+          value = 'ERROR';
+        } else {
+          memory.push(item);
+          value = getObject(elem, memory);
+          memory.pop();
+        }
+      } else {
+        value = $elem.itemValue();
+      }
+      $.each($elem.itemProp(), function(i, prop) {
+        if (!result.properties[prop])
+          result.properties[prop] = [];
+        result.properties[prop].push(value);
+      });
+    });
+    return result;
+  }
+
+  var result = {};
+  result.items = [];
+  var $items = selector ? $(selector) : $(document).items();
+  $items.each(function(i, item) {
+    var $item = $(item);
+    if ($item.itemScope())
+      result.items.push(getObject(item, []));
+  });
+  return format ? format(result) : JSON.stringify(result);
+};
