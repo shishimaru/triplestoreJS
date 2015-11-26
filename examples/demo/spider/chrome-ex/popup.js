3270986c3a9310d7d21d8873937f9b7724287eb4
Hitoshi Uchida <uchida@w3.org>
2015-11-27 00:51:38 +0900

    Included nend ads in a popup page.

--- examples/demo/spider/chrome-ex/popup.js
+++ examples/demo/spider/chrome-ex/popup.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
   },
   function(tabs) {
     var tab = tabs.length === 0 ? tabs : tabs[0];
-    
+
     var bg = chrome.extension.getBackgroundPage();
     m = bg.bg_res.m;
     m.init(tab);
@@ -17,3 +17,6 @@ document.addEventListener('DOMContentLoaded', function () {
     //v.resetItems();
   });
 });
+
+//nend ads
+var nend_params = {"media":33539,"site":175698,"spot":486874,"type":1,"oriented":1};
